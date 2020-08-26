deacfce5a3909b8ab938431c27c941e96bcef8b0
Dhrumit Mandalia <dhrumitmandaliya26@gmail.com>
2020-08-26 13:59:53 +0530

    opencv

---
+++ Test.java
@@ -0,0 +1,7 @@
+import jdk.javadoc.internal.doclets.formats.html.SourceToHTMLConverter;
+
+class Test{
+    public static void main(String[] args) {
+        System.out.println("hello World");
+    }
+}
---
+++ image.py
@@ -0,0 +1,4 @@
+import cv2
+img = cv2.imread('party.jpeg')
+cv2.imshow('rio',img)
+cv2.waitKey(0) 
